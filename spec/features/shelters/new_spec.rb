d41b0195b92a75357256d4adef40f42cd668db5f
Eugene Theriault <hybridbassist@gmail.com>
2020-10-05 18:22:13 -0700

    Create test for submitting new shelters

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe "shelter creation page", type: :feature do
     expect(page).to have_field('state')
     expect(page).to have_field('zip')
   end
+
+  it "can create a new shelter" do
+    visit '/shelters/new'
+
+    fill_in :name, with: "Happy pet house"
+    fill_in :address, with: "123 The Pets Lane"
+    fill_in :city, with: "Magical pet palace"
+    fill_in :state, with: "Bliss"
+    fill_in :zip, with: "00100"
+    click_button("Create Shelter")
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to have_content("Happy pet house")
+    expect(page).to have_content("123 The Pets Lane")
+    expect(page).to have_content("Magical pet palace")
+    expect(page).to have_content("Bliss")
+    expect(page).to have_content("00100")
+  end
 end
