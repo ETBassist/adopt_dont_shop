3d1b1f6af24b2b861da009aa41ebc12bc157e32c
Eugene Theriault <hybridbassist@gmail.com>
2020-10-06 12:21:23 -0700

    Update test with more examples

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "it can show and index of pets", type: :feature do 
-  it "can show all pets in the system" do
-    pet = Pet.create!({
+RSpec.describe "pets index page", type: :feature do 
+  before :each do
+    @pet1 = Pet.create!({
       image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
       name: "Fluffy boi",
       approximate_age: 1,
@@ -10,11 +10,26 @@ RSpec.describe "it can show and index of pets", type: :feature do
       sheltered_at: "Happy Shelter"
     })
 
+    @pet2 = Pet.create!({
+      image: "https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg",
+      name: "Apollo Cat",
+      approximate_age: 2,
+      sex: "Female",
+      sheltered_at: "SFSPCA"
+    })
+  end
+
+  it "can show all pets in the system" do
     visit '/pets'
     find("img[src='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
     expect(page).to have_content("Fluffy boi")
     expect(page).to have_content("1")
     expect(page).to have_content("Male")
     expect(page).to have_content("Happy Shelter")
+    find("https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg")
+    expect(page).to have_content("Apollo Cat")
+    expect(page).to have_content("2")
+    expect(page).to have_content("Female")
+    expect(page).to have_content("SFSPCA")
   end
 end
