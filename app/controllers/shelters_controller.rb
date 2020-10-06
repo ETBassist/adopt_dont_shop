f2268a70395a66983578bb9ad8510bfa79432301
Eugene Theriault <hybridbassist@gmail.com>
2020-10-05 22:14:43 -0700

    Create destroy method in Shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -37,4 +37,10 @@ class SheltersController < ApplicationController
     })
     redirect_to "/shelters/#{shelter.id}"
   end
+
+  def destroy
+    shelter = Shelter.find(params[:id])
+    shelter.destroy
+    redirect_to '/shelters'
+  end
 end
