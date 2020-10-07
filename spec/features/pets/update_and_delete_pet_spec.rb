2263238c301b7963c0c265875e36e1ea78e2498e
Eugene Theriault <hybridbassist@gmail.com>
2020-10-06 19:09:42 -0700

    Create test for updating details of a pet

--- spec/features/pets/update_and_delete_pet_spec.rb
+++ spec/features/pets/update_and_delete_pet_spec.rb
@@ -28,4 +28,17 @@ RSpec.describe "Update and Delete Pets", type: :feature do
     expect(page).to have_field("new_approximate_age", with: @pet.approximate_age)
     expect(page).to have_field("new_sex", with: @pet.sex)
   end
+
+  it "can update a pet" do
+    visit "pets/#{@pet.id}/edit"
+
+    fill_in :new_name, with: "Snickerdoodle"
+    fill_in :new_description, with: "A most adorable kitten, still wearing a hat but now also staring deeply into your soul"
+    click_button "Update Pet"
+
+    expect(current_path).to eq("/pets/#{@pet.id}")
+    expect(page).to have_content("Snickerdoodle")
+    expect(page).to have_content("A most adorable kitten, still wearing a hat but now also staring deeply into your soul")
+  end
 end
+
