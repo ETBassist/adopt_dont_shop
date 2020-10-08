9fce60c4312683a38feb686e9c035bda6197de69
Eugene Theriault <hybridbassist@gmail.com>
2020-10-08 11:46:42 -0700

    Create test for shelters index page link from pets show page

--- spec/features/pets/show_pet_spec.rb
+++ spec/features/pets/show_pet_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Pet Details", type: :feature do
-  it "can show details about a specific pet" do
+  before :each do
     shelter = Shelter.create!({
       name: "SFSPCA"
     })
 
-    pet = Pet.create!({
+    @pet = Pet.create!({
       image: "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg",
       name: "Ms. Piggy",
       description: "The cutest piggy you ever did see, what an adorable little snout!",
@@ -15,12 +15,22 @@ RSpec.describe "Pet Details", type: :feature do
       shelter_id: shelter.id,
       adoption_status: "Adoptable"
     })
+  end
 
-    visit "/pets/#{pet.id}"
-    expect(page).to have_content(pet.name)
-    expect(page).to have_content(pet.description)
-    expect(page).to have_content(pet.approximate_age)
-    expect(page).to have_content(pet.sex)
-    expect(page).to have_content(pet.adoption_status)
+  it "can show details about a specific pet" do
+    visit "/pets/#{@pet.id}"
+    expect(page).to have_content(@pet.name)
+    expect(page).to have_content(@pet.description)
+    expect(page).to have_content(@pet.approximate_age)
+    expect(page).to have_content(@pet.sex)
+    expect(page).to have_content(@pet.adoption_status)
+  end
+
+  it 'has a link in the nav bar to the shelters index page' do
+    visit "/pets/#{@pet.id}"
+    expect(page).to have_link('Shelters Index')
+
+    click_link('Shelters Index')
+    expect(current_path).to eq('/shelters')
   end
 end
