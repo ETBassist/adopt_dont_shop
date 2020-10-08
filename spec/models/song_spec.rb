5c279d24d8d77c3cd69c9716b5528360e8ca8d16
Eugene Theriault <hybridbassist@gmail.com>
2020-10-08 16:36:47 -0700

    Create test to validate that a pet has a name

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,4 +4,8 @@ describe Pet, type: :model do
   describe "Relationships" do
     it { should belong_to :shelter }
   end
+
+  describe "Validations" do
+    it { should validate_presence_of :name }
+  end
 end
