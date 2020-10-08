3a83900ab5d581a0df79b167849c54d32d46d457
Eugene Theriault <hybridbassist@gmail.com>
2020-10-08 14:44:16 -0700

    Create test to check for nav links on shelters#pet_index view

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -35,55 +35,69 @@ RSpec.describe "pets index page", type: :feature do
     })
   end
 
-  it "can show all pets in the system" do
-    visit '/pets'
-    find("img[src='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
-    expect(page).to have_content(@pet1.name)
-    expect(page).to have_content(@pet1.approximate_age)
-    expect(page).to have_content(@pet1.sex)
-    expect(page).to have_content(@shelter1.name)
-    find("img[src='https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg']")
-    expect(page).to have_content(@pet2.name)
-    expect(page).to have_content(@pet2.approximate_age)
-    expect(page).to have_content(@pet2.sex)
-    expect(page).to have_content(@shelter1.name)
-  end
+  describe "All Pets Index" do
+    it "can show all pets in the system" do
+      visit '/pets'
+      find("img[src='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
+      expect(page).to have_content(@pet1.name)
+      expect(page).to have_content(@pet1.approximate_age)
+      expect(page).to have_content(@pet1.sex)
+      expect(page).to have_content(@shelter1.name)
+      find("img[src='https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg']")
+      expect(page).to have_content(@pet2.name)
+      expect(page).to have_content(@pet2.approximate_age)
+      expect(page).to have_content(@pet2.sex)
+      expect(page).to have_content(@shelter1.name)
+    end
 
-  it "can view pets at a specific shelter" do
-    visit "/shelters/#{@shelter1.id}/pets"
-    find("img[src='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
-    expect(page).to have_content(@pet1.name)
-    expect(page).to have_content(@pet1.approximate_age)
-    expect(page).to have_content(@pet1.sex)
-    expect(page).to have_content(@shelter1.name)
-    find("img[src='https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg']")
-    expect(page).to have_content(@pet2.name)
-    expect(page).to have_content(@pet2.approximate_age)
-    expect(page).to have_content(@pet2.sex)
-    expect(page).to have_content(@shelter1.name)
+    it "has nav link to shelter index from pets index" do
+      visit "/pets/"
 
-    expect(page).to have_no_content(@pet3.name)
-    expect(page).to have_no_content(@pet3.approximate_age)
-    expect(page).to have_no_content(@shelter2.name)
-  end
+      within('nav') do
+        expect(page).to have_link("Shelters Index")
+        click_link("Shelters Index")
+        expect(current_path).to eq("/shelters")
+      end
+    end
 
-  it "has nav link to shelter index from pets index" do
-    visit "/pets/"
+    it "has navbar link to pet index from within pets index" do
+      visit "/pets"
 
-    within('nav') do
-      expect(page).to have_link("Shelters Index")
-      click_link("Shelters Index")
-      expect(current_path).to eq("/shelters")
+      within('nav') do
+        expect(page).to have_link("Pets Index")
+        click_link("Pets Index")
+        expect(current_path).to eq("/pets")
+      end
     end
   end
 
-  it "has navbar link to pet index from within pets index" do
-    visit "/pets"
+  describe "Shelters Pet Index" do
+    it "can view pets at a specific shelter" do
+      visit "/shelters/#{@shelter1.id}/pets"
+      find("img[src='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
+      expect(page).to have_content(@pet1.name)
+      expect(page).to have_content(@pet1.approximate_age)
+      expect(page).to have_content(@pet1.sex)
+      expect(page).to have_content(@shelter1.name)
+      find("img[src='https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg']")
+      expect(page).to have_content(@pet2.name)
+      expect(page).to have_content(@pet2.approximate_age)
+      expect(page).to have_content(@pet2.sex)
+      expect(page).to have_content(@shelter1.name)
 
-    within('nav') do
-      expect(page).to have_link("Pets Index")
-      click_link("Pets Index")
-      expect(current_path).to eq("/pets")
+      expect(page).to have_no_content(@pet3.name)
+      expect(page).to have_no_content(@pet3.approximate_age)
+      expect(page).to have_no_content(@shelter2.name)
+    end
+
+    it "has a nav link to shelter index from shelters pets index" do
+      visit "/shelters/#{@shelter.id}/pets"
+
+      within('nav') do
+        expect(page).to have_link("Shelters Index")
+        click_link("Shelters Index")
+        expect(current_path).to eq("/shelters")
+      end
     end
   end
 end
