b895eed4ed8d6699bb1bba4f141295af328734cb
Eugene Theriault <hybridbassist@gmail.com>
2020-10-08 15:17:28 -0700

    Create test to delete pet from shelters#pets_index

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -10,28 +10,25 @@ RSpec.describe "pets index page", type: :feature do
       name: "Happy Furballs"
     })
 
-    @pet1 = Pet.create!({
+    @pet1 = @shelter1.pets.create!({
       image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
       name: "Fluffy boi",
       approximate_age: 1,
-      sex: "Male",
-      shelter_id: @shelter1.id
+      sex: "Male"
     })
 
-    @pet2 = Pet.create!({
+    @pet2 = @shelter1.pets.create!({
       image: "https://filmdaily.co/wp-content/uploads/2020/04/cute-cat-videos-lede-1300x882.jpg",
       name: "Apollo Cat",
       approximate_age: 2,
-      sex: "Female",
-      shelter_id: @shelter1.id
+      sex: "Female"
     })
 
-    @pet3 = Pet.create!({
+    @pet3 = @shelter2.pets.create!({
       image: "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg",
       name: "Cute Piggledy",
       approximate_age: 5,
-      sex: "Male",
-      shelter_id: @shelter2.id
+      sex: "Male"
     })
   end
 
@@ -83,7 +80,6 @@ RSpec.describe "pets index page", type: :feature do
       expect(page).to have_content(@pet2.name)
       expect(page).to have_content(@pet2.approximate_age)
       expect(page).to have_content(@pet2.sex)
-      expect(page).to have_content(@shelter1.name)
 
       expect(page).to have_no_content(@pet3.name)
       expect(page).to have_no_content(@pet3.approximate_age)
@@ -108,6 +104,16 @@ RSpec.describe "pets index page", type: :feature do
         click_link("Pets Index")
         expect(current_path).to eq("/pets")
       end
+    end
+
+    it "has a button to delete a pet from the shelters pet index" do
+      visit "/shelters/#{@shelter1.id}/pets"
+
+      expect(page).to have_button("Delete #{@pet1.name}")
+      click_button("Delete #{@pet1.name}")
+      expect(current_path).to eq("/pets")
+
+      expect(page).to have_no_content("#{@pet1.name}")
     end
   end
 end
