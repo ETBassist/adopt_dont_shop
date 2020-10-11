46e2ce97e4deed4fcc5700e396f9110650a479df
Eugene Theriault <hybridbassist@gmail.com>
2020-10-11 11:50:47 -0700

    Create sorting methods for name and number of adoptable pets

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,16 @@
 class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
+
+  def self.by_name
+    Shelter.order(:name)
+  end
+
+  def self.by_num_adoptable_pets
+    shelters = Shelter.all
+    shelters.sort_by do |shelter|
+      shelter.pets.count do |pet|
+        pet.adoption_status.current_status == "Adoptable"
+      end
+    end.reverse
+  end
 end
