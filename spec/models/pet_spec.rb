3c9b26a86949030dd4180f6f35001ada4a215a3e
Eugene Theriault <hybridbassist@gmail.com>
2020-10-11 12:56:10 -0700

    Create test for ordering pets by their adoption status

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -13,14 +13,17 @@ describe Pet, type: :model do
 
   it "can sort pets by adoption status" do
     shelter = Shelter.create!({name: "SFSPCA"})
+    shelter2 = Shelter.create!({name: "The Other Place"})
     pet1 = shelter.pets.create!({name: "Benny",
                        adoption_status: PendingAdoption.new})
     pet2 = shelter.pets.create!({name: "Biggy",
                        adoption_status: NullAdoption.new})
-    original_order = [pet1, pet2]
+    pet3 = shelter2.pets.create!({name: "Buggy",
+                                  adoption_status: NullAdoption.new})
+    original_order = [pet1, pet2, pet3]
     expect(Pet.all).to eq(original_order)
 
     ordered_by_status = [pet2, pet1]
-    expect(Pet.by_status).to eq(ordered_by_status)
+    expect(shelter.pets.by_status).to eq(ordered_by_status)
   end
 end
